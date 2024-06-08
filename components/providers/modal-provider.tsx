20c1dab36fe2bafda355627bd4acead7869036c1
Du Cheng <easyhoo2426@163.com>
2024-06-09 04:17:38 +0800

    feat: delete message

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react'
 import CreateChannelModal from '~/components/modals/create-channel-modal'
 import CreateServerModal from '~/components/modals/create-server-modal'
 import DeleteChannelModal from '~/components/modals/delete-channel-modal'
+import DeleteMessageModal from '~/components/modals/delete-message'
 import DeleteServerModal from '~/components/modals/delete-server-modal'
 import EditChannelModal from '~/components/modals/edit-channel-modal'
 import EditServerModal from '~/components/modals/edit-server-modal'
@@ -33,6 +34,7 @@ export const ModalProvider = () => {
       <DeleteServerModal />
       <DeleteChannelModal />
       <EditChannelModal />
+      <DeleteMessageModal />
     </>
   )
 }
